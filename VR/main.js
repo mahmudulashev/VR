1a35341189df152899502a641ab886dbdfe4d614
Alpha <111943905+mahmudulashev@users.noreply.github.com>
2025-03-10 18:26:02 +0500

    Delete VR directory

--- VR/main.js
+++
@@ -1,8 +0,0 @@
-const hamMenu = document.querySelector('.ham-menu');
-
-const offScreen = document.querySelector('.off-screen-menu');
-
-hamMenu.addEventListener('click', () => {
-    hamMenu.classList.toggle('active');     
-    offScreen.classList.toggle('active');
-});
